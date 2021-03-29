dec5168ea01f137243872eee59f0d88c7682099d
DinSmol <dinar184@mail.ru>
2021-03-29 22:43:38 +0300

    fixed count value in stat controller

--- library/controllers/stat_controller.rb
+++ library/controllers/stat_controller.rb
@@ -9,7 +9,7 @@ class StatController < ApplicationController
 
     query = "SELECT MIN(sd.min), AVG(sd.avg), MAX(sd.max), PERCENTILE_DISC(0.5) WITHIN GROUP (ORDER BY sd.avg) as median_score,
 STDDEV(sd.avg) as st_avg, COUNT(sd.address) as cnt, (COUNT(CASE WHEN sd.loss = 100 THEN 1 END)/COUNT(sd.address))*100 as loss
-FROM stat_data sd WHERE sd.address='#{address}' AND (sd.created_at BETWEEN '#{from}' AND '#{to}')"
+FROM stat_data sd WHERE sd.address='#{address}' AND (sd.created_at BETWEEN '#{from}' AND '#{to}' AND sd.avg IS NOT NULL)"
     begin
       data = StatObject.sql_exec(query).to_a
     rescue ActiveRecord::StatementInvalid
